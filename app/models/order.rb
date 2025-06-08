58f887be6010b23755742467add0b3cd969b623d
dfdsf dfdsf <alex.ostapenko.2018@gmail.com>
2025-06-08 20:29:17 +0300

    Добавил визуалы для большинства страниц

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items, dependent: :destroy
-
   validates :name, :address, :phone, presence: true
+  def total_price
+    order_items.includes(:product).sum do |item|
+      item.quantity * item.product.price
+    end
+  end
 end
